1922fc723f2006d3b1bc373da9271f022d2eaaee
Michael Bishop <cleverca22@gmail.com>
2019-10-10 00:36:16 -0300

    add an example daedalus-bridge

--- default.nix
+++ default.nix
@@ -1,14 +1,25 @@
-{ system ? builtins.currentSystem
-, crossSystem ? null
-, config ? {}
-# Import IOHK common nix lib
-, iohkLib ? import ./nix/iohk-common.nix { inherit system crossSystem config; }
-# Use nixpkgs pin from iohkLib
-, pkgs ? iohkLib.pkgs
+{ config ? {}
+, target ? builtins.currentSystem
 }:
 
-with import ./nix/util.nix { inherit pkgs; };
 
+let
+  commonLib = import ./nix/iohk-common.nix {};
+  lib = commonLib.pkgs.lib;
+  systemTable = {
+    x86_64-windows = builtins.currentSystem;
+  };
+  crossSystemTable = {
+    x86_64-windows = lib.systems.examples.mingwW64;
+  };
+  system = systemTable.${target} or target;
+  crossSystem = crossSystemTable.${target} or null;
+  # Import IOHK common nix lib
+  iohkLib = import ./nix/iohk-common.nix { inherit system crossSystem config; };
+  # Use nixpkgs pin from iohkLib
+  pkgs = iohkLib.pkgs;
+in
+with import ./nix/util.nix { inherit pkgs; };
 let
   haskell = iohkLib.nix-tools.haskell { inherit pkgs; };
   src = iohkLib.cleanSourceHaskell ./.;
@@ -38,6 +49,8 @@ in {
 
   tests = collectComponents "tests" isCardanoWallet haskellPackages;
   benchmarks = collectComponents "benchmarks" isCardanoWallet haskellPackages;
+
+  daedalus-bridge = import ./daedalus-bridge.nix { inherit target pkgs haskellPackages system crossSystem jormungandr; };
 
   shell = haskellPackages.shellFor {
     name = "cardano-wallet-shell";
